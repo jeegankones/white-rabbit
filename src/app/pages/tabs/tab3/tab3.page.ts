3a4b5b88f0c578bd29eb61b55dbab21befc385c1
KeeganJones <keegan.jones@braviant.com>
2022-07-07 11:01:17 -0500

    Move tabs files to new dir

--- src/app/pages/tabs/tab3/tab3.page.ts
+++ src/app/pages/tabs/tab3/tab3.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertController, LoadingController } from '@ionic/angular';
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
